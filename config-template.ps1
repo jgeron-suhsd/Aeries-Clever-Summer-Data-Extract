90f4643d3eebf908cf69614ebc2a45b0e3cd5402
Jesse Geron <67440789+jgeron-suhsd@users.noreply.github.com>
2023-05-17 08:03:48 -0700

    Add teacher/student dataexport csv variables

--- config-template.ps1
+++ config-template.ps1
@@ -5,10 +5,15 @@ $AeriesUrl = 'https://aeries.school.com/'
 $DbYear = '2021' # To always get current year, use $DbYear = (Get-Date).Year.ToString()
                  # In testing before you have data ready, you may want to try pulling last year's data
 
-#retrieve Aeries API Key from key vault
-$AeriesApiKey = get-secret -Name 'AeriesCleverSummerExtractApiKey'
+# Download all student and teacher records from the Clever data browser
+# CSV of existing teachers in Clever to exclude from export
+$CleverTeachers = Get-Content -Path "$ExportPath\teachers.csv" | ConvertFrom-Csv
+# CSV of existing students in Clever to exclude from export
+$CleverStudents = Get-Content -Path "$ExportPath\students.csv" | ConvertFrom-Csv
 
 
+# Retrieve Aeries API Key from key vault
+$ AeriesApiKey = get-secret -Name 'AeriesCleverSummerExtractApiKey'
 # To store the cert string in your vault using PowerShell SecretManagement, use the following command:
 # $ApiKey = 'APIKeyFromAeriesHere' | ConvertTo-SecureString -AsPlainText -Force
 # Set-Secret -Vault 'MyVault' -Name 'AeriesCleverSummerExtract' -Secret $ApiKey
