d8dbf7b65743c1fa97ce44e9c8f0359577698d92
Jesse Geron <jgeron@suhsd.net>
2024-06-06 11:06:55 -0700

    add multiple schools and section prefix

--- Invoke-AeriesCleverSummerExtract.ps1
+++ Invoke-AeriesCleverSummerExtract.ps1
@@ -1,9 +1,12 @@
 #Requires -Modules AeriesApi, Microsoft.PowerShell.SecretManagement
 
-# paramaterize script to take location of config file, otherwise, we'll assume it's in the same directory
-param (
-    [string]$ConfigPath = '.\config.ps1'
-)
+# Load Config
+. .\config.ps1
+
+# ouput basic information from config
+Write-Host "Export Path: $ExportPath"
+Write-Host "Aeries URL: $AeriesUrl"
+Write-Host "Database Year: $DbYear"
 
 # Create Export Directory
 $Timestamp = Get-Date -Format 'yyyy-MM-dd_HH-mm-ss'
@@ -26,145 +29,164 @@ $CleverStudentHT = @{}
 $CleverStudents | ForEach-Object {$CleverStudentHT.Add($_.student_sis_id, $_)}
 Write-Host "Existing Clever Students: $($CleverStudentHT.Count)"
 
-
 # Initialize Aeries API
 Initialize-AeriesApi -URL $AeriesUrl -Certificate ($AeriesApiKey | ConvertFrom-SecureString -AsPlainText) -DatabaseYear $DbYear
 
-# Get Summer School Data from Aeries
-$SummerSchool = Get-AeriesSchool | Where-Object {$_.Name -like $SummerSchoolName}
-$SummerStudents = Get-AeriesStudent -SchoolCode $SummerSchool.SchoolCode
-$SummerTeachers = Get-AeriesTeacher -SchoolCode $SummerSchool.SchoolCode
-$SummerSections = Get-AeriesSection -SchoolCode $SummerSchool.SchoolCode
-$SummerRosters = $SummerSections | ForEach-Object {Get-AeriesSectionRoster -SchoolCode $SummerSchool.SchoolCode -SectionNumber $_.SectionNumber}
-Write-Host "Summer School Students: $($SummerStudents.Count)"
-Write-Host "Summer School Teachers: $($SummerTeachers.Count)"
-Write-Host "Summer School Sections: $($SummerSections.Count)"
-Write-Host "Summer School Rosters: $($SummerRosters.Count)"
-
 # Create HT for Courses by course ID
 $Courses = Get-AeriesCourseInformation
+
 $CourseHT = @{}
 $Courses | ForEach-Object {$CourseHT.Add($_.ID, $_)}
 
-# Format student data for Clever
-$students = $SummerStudents | ForEach-Object {
-    [pscustomobject]@{
-        School_id = $_.SchoolCode #Required
-        Student_id = $_.StudentID #Required
-        Student_number = $_.StudentID # There is also a StudentNumber property, but this is the default Aeries/Clever mapping
-        State_id = $_.StateStudentID
-        Last_name = $_.LastName #Required
-        Middle_name = $_.MiddleName
-        First_name = $_.FirstName #Required
-        Grade = $_.Grade
-        Gender = $_.Gender
-        #Graduation_year = $_.
-        DOB = $_.BirthDate
-        #Race = $_
-        #Hispanic_Latino = $_
-        Home_language = $_.HomeLanguageCode
-        #Ell_status = $_
-        #Frl_status = $_
-        #IEP_status = $_
-        Student_Street = $_.MailingAddress
-        Student_City = $_.MailingAddressCity
-        Student_State = $_.MailingAddressState
-        Student_zip = $_.MailingAddressZipCode
-        Student_email = $_.StudentEmailAddress
-        #Contact_relationship = $_
-        #Contact_type = $_
-        #Contact_name = $_
-        #Contact_phone = $_
-        #Contact_phone_type = $_
-        #Contact_email = $_
-        #Contact_sis_id = $_
-        Username = $_.StudentEmailAddress
-        Password = $_.NetworkLoginID
-        #Unweighted_gpa = $_
-        #Weighted_gpa = $_
-        #ext.* Additional data sent over in extension field.
-    }
-}
-
-# Check if student is already in Clever, if not add to $NewStudents
 $NewStudents = @()
-foreach ($student in $students) {
-    if (-not $CleverStudentHT.ContainsKey($student.Student_id.ToString())) {
-        $NewStudents += $student
-    }
-}
-Write-Host "New Students: $($NewStudents.Count)"
-# Export Student Data to CSV
-$NewStudents | export-csv -path $ExportDirectory\Students.csv -NoTypeInformation
-
-# Format teacher data for Clever
-$teachers = $SummerTeachers | ForEach-Object {
-    [pscustomobject]@{
-        School_id = $_.SchoolCode #Required
-        Teacher_id = $_.StaffID1 #Required
-        Teacher_number = $_.StaffID1
-        #State_teacher_id = $_.
-        Teacher_email = $_.EmailAddress
-        First_name = $_.FirstName #Required
-        Last_name = $_.LastName #Required
-        #Title = $_
-        #Username = $_
-        #Password = $_
-        #ext.* Additional data sent over in extension field.
-    }
-}
-# Check if teacher is already in Clever, if not add to $NewTeachers
 $NewTeachers = @()
-foreach ($teacher in $teachers) {
-    if (-not $CleverTeacherHT.ContainsKey($teacher.Teacher_id.ToString())) {
-        $NewTeachers += $teacher
-    }
-}
-Write-Host "New Teachers: $($NewTeachers.Count)"
-# Export Teacher Data to CSV
-$NewTeachers | export-csv -path $ExportDirectory\Teachers.csv -NoTypeInformation
+$Sections = @()
+$Enrollments = @()
 
-# Format section data for Clever
-$Sections = $SummerSections | ForEach-Object {
-    [pscustomobject]@{
-        School_id = $_.SchoolCode #Required
-        Section_id = $_.SectionNumber #Required
-        Teacher_id = $_.SectionStaffMembers[0].StaffID #Required
-        Teacher_2_id = $_.SectionStaffMembers[1].StaffID
-        Teacher_3_id = $_.SectionStaffMembers[2].StaffID
-        Teacher_4_id = $_.SectionStaffMembers[3].StaffID
-        Teacher_5_id = $_.SectionStaffMembers[4].StaffID
-        Teacher_6_id = $_.SectionStaffMembers[5].StaffID
-        Teacher_7_id = $_.SectionStaffMembers[6].StaffID
-        Teacher_8_id = $_.SectionStaffMembers[7].StaffID
-        Teacher_9_id = $_.SectionStaffMembers[8].StaffID
-        Teacher_10_id = $_.SectionStaffMembers[9].StaffID
-        Name = $CourseHT[$_.CourseID].Title
-        Section_number = $_.SectionNumber
-        Grade = $_.HighGrade
-        Course_name = $CourseHT[$_.CourseID].Title
-        Course_number = $_.CourseID
-        Course_description = $CourseHT[$_.CourseID].ContentDescription
-        Period = $_.Period
-        #Subject = $_ #May need to cross reference elsewhere
-        Term_name = $TermName #$_.Semester # ??? Maybe? Maybe we pull it from the school?
-        Term_start = $TermStart
-        Term_end = $TermEnd
-        #ext.* Additional data sent over in extension field.
+foreach ($Config in $Configs) {
+
+
+    # List all config values
+    Write-Host "Summer School Name: $($Config.SummerSchoolName)"
+    Write-Host "Term Name: $($Config.TermName)"
+    Write-Host "Term Start: $($Config.TermStart)"
+    Write-Host "Term End: $($Config.TermEnd)"
+    
+
+    # Get Summer School Data from Aeries
+    $SummerSchool = Get-AeriesSchool | Where-Object {$_.Name -like $Config.SummerSchoolName}
+    $SummerStudents = Get-AeriesStudent -SchoolCode $SummerSchool.SchoolCode
+    $SummerTeachers = Get-AeriesTeacher -SchoolCode $SummerSchool.SchoolCode
+    $SummerSections = Get-AeriesSection -SchoolCode $SummerSchool.SchoolCode
+    $SummerRosters = $SummerSections | ForEach-Object {Get-AeriesSectionRoster -SchoolCode $SummerSchool.SchoolCode -SectionNumber $_.SectionNumber}
+    Write-Host "Summer School Students: $($SummerStudents.Count)"
+    Write-Host "Summer School Teachers: $($SummerTeachers.Count)"
+    Write-Host "Summer School Sections: $($SummerSections.Count)"
+    Write-Host "Summer School Rosters: $($SummerRosters.Count)"
+
+    # Format student data for Clever
+    $students = $SummerStudents | ForEach-Object {
+        [pscustomobject]@{
+            School_id = $_.SchoolCode #Required
+            Student_id = $_.StudentID #Required
+            Student_number = $_.StudentID # There is also a StudentNumber property, but this is the default Aeries/Clever mapping
+            State_id = $_.StateStudentID
+            Last_name = $_.LastName #Required
+            Middle_name = $_.MiddleName
+            First_name = $_.FirstName #Required
+            Grade = $_.Grade
+            Gender = $_.Gender
+            #Graduation_year = $_.
+            DOB = $_.BirthDate
+            #Race = $_
+            #Hispanic_Latino = $_
+            Home_language = $_.HomeLanguageCode
+            #Ell_status = $_
+            #Frl_status = $_
+            #IEP_status = $_
+            Student_Street = $_.MailingAddress
+            Student_City = $_.MailingAddressCity
+            Student_State = $_.MailingAddressState
+            Student_zip = $_.MailingAddressZipCode
+            Student_email = $_.StudentEmailAddress
+            #Contact_relationship = $_
+            #Contact_type = $_
+            #Contact_name = $_
+            #Contact_phone = $_
+            #Contact_phone_type = $_
+            #Contact_email = $_
+            #Contact_sis_id = $_
+            Username = $_.StudentEmailAddress
+            Password = $_.NetworkLoginID
+            #Unweighted_gpa = $_
+            #Weighted_gpa = $_
+            #ext.* Additional data sent over in extension field.
+        }
+    }
+
+    # Check if student is already in Clever, if not add to $NewStudents
+    foreach ($student in $students) {
+        if (-not $CleverStudentHT.ContainsKey($student.Student_id.ToString())) {
+            $NewStudents += $student
+        }
+    }
+    Write-Host "New Students: $($NewStudents.Count)"
+
+    # Format teacher data for Clever
+    $teachers = $SummerTeachers | ForEach-Object {
+        [pscustomobject]@{
+            School_id = $_.SchoolCode #Required
+            Teacher_id = $_.StaffID1 #Required
+            Teacher_number = $_.StaffID1
+            #State_teacher_id = $_.
+            Teacher_email = $_.EmailAddress
+            First_name = $_.FirstName #Required
+            Last_name = $_.LastName #Required
+            #Title = $_
+            #Username = $_
+            #Password = $_
+            #ext.* Additional data sent over in extension field.
+        }
+    }
+    # Check if teacher is already in Clever, if not add to $NewTeachers
+    foreach ($teacher in $teachers) {
+        if (-not $CleverTeacherHT.ContainsKey($teacher.Teacher_id.ToString())) {
+            $NewTeachers += $teacher
+        }
+    }
+    Write-Host "New Teachers: $($NewTeachers.Count)"
+    
+
+    # Format section data for Clever
+    $Sections += $SummerSections | ForEach-Object {
+        [pscustomobject]@{
+            School_id = $_.SchoolCode #Required
+            Section_id = "$($config.SectionPrefix)$($_.SectionNumber)" #Required
+            Teacher_id = $_.SectionStaffMembers[0].StaffID #Required
+            Teacher_2_id = $_.SectionStaffMembers[1].StaffID
+            Teacher_3_id = $_.SectionStaffMembers[2].StaffID
+            Teacher_4_id = $_.SectionStaffMembers[3].StaffID
+            Teacher_5_id = $_.SectionStaffMembers[4].StaffID
+            Teacher_6_id = $_.SectionStaffMembers[5].StaffID
+            Teacher_7_id = $_.SectionStaffMembers[6].StaffID
+            Teacher_8_id = $_.SectionStaffMembers[7].StaffID
+            Teacher_9_id = $_.SectionStaffMembers[8].StaffID
+            Teacher_10_id = $_.SectionStaffMembers[9].StaffID
+            Name = $CourseHT[$_.CourseID].Title
+            Section_number = $_.SectionNumber
+            Grade = $_.HighGrade
+            Course_name = $CourseHT[$_.CourseID].Title
+            Course_number = $_.CourseID
+            Course_description = $CourseHT[$_.CourseID].ContentDescription
+            Period = $_.Period
+            #Subject = $_ #May need to cross reference elsewhere
+            Term_name = $Config.TermName #$_.Semester # ??? Maybe? Maybe we pull it from the school?
+            Term_start = $Config.TermStart
+            Term_end = $Config.TermEnd
+            #ext.* Additional data sent over in extension field.
+        }
+    }
+
+    # Format enrollment data for Clever
+    $enrollments += $SummerRosters | ForEach-Object {
+        [pscustomobject]@{
+            School_id = $_.SchoolCode #Required
+            Section_id = $_.SectionNumber #Required
+            Student_id = $_.StudentID #Required
+        }
     }
 }
+
+# Export Student Data to CSV
+$NewStudents | export-csv -path "$ExportDirectory\Students.csv" -NoTypeInformation
+
+# Export Teacher Data to CSV
+$NewTeachers | export-csv -path "$ExportDirectory\Teachers.csv" -NoTypeInformation
+
 # Export Section Data to CSV
 $Sections | export-csv -path "$ExportDirectory\Sections.csv" -NoTypeInformation -Force
 
-# Format enrollment data for Clever
-$enrollments = $SummerRosters | ForEach-Object {
-    [pscustomobject]@{
-        School_id = $_.SchoolCode #Required
-        Section_id = $_.SectionNumber #Required
-        Student_id = $_.StudentID #Required
-    }
-}
 # Export Enrollment Data to CSV
-$Enrollments | export-csv -path $ExportDirectory\Enrollments.csv -NoTypeInformation
+$Enrollments | export-csv -path "$ExportDirectory\Enrollments.csv" -NoTypeInformation
 
 Stop-Transcript
--- config-template.ps1
+++ config-template.ps1
@@ -1,19 +1,46 @@
-# Update these values for your environment and save as config.ps1
-$SummerSchoolName = 'Aeries Summer School Name'
+# Going to create an array of objects for each school in the district using all the variables in the config file.
+#
+
 $ExportPath = 'C:\temp\AeriesExtracts\SummerSchool\'
 $AeriesUrl = 'https://aeries.school.com/'
-$DbYear = '2021' # To always get current year, use $DbYear = (Get-Date).Year.ToString()
-                 # In testing before you have data ready, you may want to try pulling last year's data
+$DbYear = '2023'
 
 # Download all student and teacher records from the Clever data browser
 # CSV of existing teachers in Clever to exclude from export
-$CleverTeachers = Get-Content -Path "$ExportPath\teachers.csv" | ConvertFrom-Csv
+$CleverTeachers = Get-Content -Path "$ExportPath\CleverTeachers.csv" | ConvertFrom-Csv
 # CSV of existing students in Clever to exclude from export
-$CleverStudents = Get-Content -Path "$ExportPath\students.csv" | ConvertFrom-Csv
+$CleverStudents = Get-Content -Path "$ExportPath\CleverStudents.csv" | ConvertFrom-Csv
+
+$Configs = @(
+    @{
+        SummerSchoolName = 'Summer School 1'
+        TermName = '1st Quarter'
+        TermStart = '06-5-2024'
+        TermEnd = '07-22-2024'
+        SectionPrefix = '770_S' # Section IDs need to be unique in Clever, prefix should be the school code _ S for summer
+                                # Can work this into the core code in the future, but have to do it this way for now.
+    }
+    # multiple hashtables can be added here for each summer school "school" in Aeries.
+    @{
+        SummerSchoolName = 'Summer School 2'
+        TermName = '1st Quarter'
+        TermStart = '06-12-2024'
+        TermEnd = '07-10-2024'
+        SectionPrefix = ''
+    }
+)
+
+# Update these values for your environment and save as config.ps1
+#$SummerSchoolName = 'Summer School'
+#$ExportPath = 'C:\Temp\2024-Clever-Summer-School\'
+#$AeriesUrl = 'https://aeries.school.com/'
+#$DbYear = '2023' # To always get current year, use $DbYear = (Get-Date).Year.ToString()
+                 # In testing before you have data ready, you may want to try pulling last year's data
 
 
-# Retrieve Aeries API Key from key vault
-$ AeriesApiKey = get-secret -Name 'AeriesCleverSummerExtractApiKey'
+
+#retrieve Aeries API Key from key vault
+$AeriesApiKey = get-secret -Name 'AeriesCleverSummerExtractApiKey'
 # To store the cert string in your vault using PowerShell SecretManagement, use the following command:
 # $ApiKey = 'APIKeyFromAeriesHere' | ConvertTo-SecureString -AsPlainText -Force
 # Set-Secret -Vault 'MyVault' -Name 'AeriesCleverSummerExtract' -Secret $ApiKey
@@ -22,6 +49,6 @@ $ AeriesApiKey = get-secret -Name 'AeriesCleverSummerExtractApiKey'
 # Hardcoding this into the config, as this is needed and need a last minute fix.
 # Our district only has 1 term for summer school, so should be straight forward.
 # If a district has multiple terms, would need to associate the correct term with each section in the main script.
-$TermName = 'Summer' # Run Get-AeriesSchoolTerms -SchoolCode $SummerSchool.SchoolCode to get the correct term name
-$TermStart = '06-13-2023' # Should be in MM-DD-YYYY format.
-$TermEnd = '07-13-2023' # Should be in MM-DD-YYYY format.
+#$TermName = '1st Quarter' # Run Get-AeriesSchoolTerms -SchoolCode $SummerSchool.SchoolCode to get the correct term name
+#$TermStart = '06-5-2024' # Should be in MM-DD-YYYY format.
+#$TermEnd = '07-22-2024' # Should be in MM-DD-YYYY format.
